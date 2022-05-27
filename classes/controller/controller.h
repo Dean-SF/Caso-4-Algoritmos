f45b3d1269f70d46550c99b224bb060e6dce5195
Dean-SF <dsanabriaf@estudiantec.cr>
2022-05-26 20:21:57 -0600

    backup

---
+++ classes/controller/controller.h
@@ -0,0 +1,14 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "../probabilistic/ProbabilisticAlgorithm.h"
+
+class controller {
+private:
+    
+public:
+    controller(/* args */);
+    ~controller();
+};
+
+#endif
